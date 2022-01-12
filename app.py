73a026a88ab5117a58c945d6bc53f18c67f45f86
Nizar Baihaqi <n1z4rbaihaqi@webmail.umm.ac.id>
2022-01-12 18:09:44 +0700

    change model

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
 model = load_model('model_klasifikasi_gambar_tikus_hamster.h5')
 
-class_dict = {0: 'Tikus', 1: 'Hamster'}
+class_dict = {0: 'Hamster', 1: 'Tikus'}
 
 def predict_label(img_path):
     query = cv2.imread(img_path)
